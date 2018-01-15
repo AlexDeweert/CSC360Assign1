1c5dacd30b4c591d559842aea06eae2cb937d759
Alex <alexander.deweert@gmail.com>
2018-01-15 02:29:26 +0000

    Command parsing puts all strings into an array with a presumed null terminator at the end. Haven't yet implemented fork() or execvp()

--- sample.c
+++ sample.c
@@ -9,6 +9,7 @@
 
 /*Prototypes*/
 char* getWorkingDirectory();
+void parseInput(char*);
 
 int main()
 {
@@ -17,20 +18,18 @@ int main()
 	int bailout = 0;
 	while (!bailout) {
 		prompt = getWorkingDirectory();
-		char* reply = readline(prompt);
+		char* input = readline(prompt);
 		/* Note that readline strips away the final \n */
 
-		if (!strcmp(reply, "bye")) {
+		if (!strcmp(input, "bye")) {
 			bailout = 1;
 		}
-		else if ( !strcmp(reply, "pwd")) {
-			getWorkingDirectory();
-		}
 		else {
-			printf("\nYou said: %s\n\n", reply);
+			parseInput(input);
+			//printf("%s: command not found\n", input);
 		}
 		
-		free(reply);
+		free(input);
 		free(prompt);
 		
 	}
@@ -38,6 +37,77 @@ int main()
 	printf("Bye Bye\n");
 }
 
+/* parseIput( char* )
+*  separating command line input into usable arguments
+*  likely to be fed into execvp() system call
+*/
+void parseInput( char* input ) {
+
+	/*vars*/
+	size_t size_args = 1;
+	int num_tokens = 0;
+
+	/*Read the string and tokenize it into individual words delimited by spaces*/
+	char* token;
+	char** args = (char**)malloc( sizeof(char*)*size_args );
+
+	const char* delimeter = " ";
+
+	/*First token*/
+	token = strtok( input, delimeter );
+	/*Remaining tokens*/
+	while( token != NULL ) {
+		printf( "TOKEN IS: %s\n", token );
+
+		//Put each token into the args array
+		//Grow args if tokens will exceed the length of array
+		if( (num_tokens+1) > size_args ) {
+			printf("num_tokens exceeded the size of the argument array\n");
+			size_args++;
+			printf("size args: %zu\n", size_args);
+			args = (char**)realloc( args, sizeof(char*)*size_args );
+		}
+		printf("attempting to malloc\n");
+		args[num_tokens] = (char*)malloc( sizeof(char)*(strlen(token)+1) );
+		strncpy( args[num_tokens], token, strlen(token)+1 );
+		num_tokens++;
+		token = strtok( NULL, delimeter );
+	}
+	size_args++;
+	/*Try to null terminate the array of strings*/
+	args = (char**)realloc( args, sizeof(NULL)*(size_args) );
+	printf("Size of null is: %zu, size of char* is: %zu, num_tokens: %d, size_args: %zu\n", sizeof(NULL), sizeof(char*), num_tokens, size_args);
+	args[size_args-1] = NULL;
+
+	int i;
+	for( i = 0; i < num_tokens; i++ ) {
+		printf("args at %d is: %s with size %zu\n", i, args[i], strlen( args[i]));
+	}
+
+	//Determine if the very first word is a "valid" command - ie will be args[0]
+	if( strcmp( args[0], "ls" ) == 0) {
+		printf("Got command \"ls\"\n");
+	}
+
+	
+
+	//args[1...n-1] will be the options
+
+	//args[n] = NULL to indicate end of args
+
+	for( i = 0; i < size_args; i++) {
+		free( args[i] );
+	}
+	free( args );
+}
+
+
+/* getWorkingDirectory()
+*  This function uses the system call getcwd() and appends it
+*  to a default "simple shell" prompt. The fn returns a string
+*  which is typically printed out in a loop that waits for
+*  user input commands.
+*/
 char* getWorkingDirectory() {
 	char* buf = NULL;
 	/*Note that ptr_buf points to the actual string*/
@@ -75,9 +145,6 @@ char* getWorkingDirectory() {
 			allocated = 1; 
 		}
 	}
-	//printf("\nptr_buf size: %zu\nbuf size: %zu\n", strlen(ptr_buf), sizeof(buf));
-	//strncpy( cwd, ptr_buf, sizeof(buf) );
 	free(ptr_buf);
 	return finalPrompt;
-	//free(finalPrompt);
 }
