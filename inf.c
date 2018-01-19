c69bd64700000486e3a55059636848c37a6df754
Alex <alexander.deweert@gmail.com>
2018-01-19 01:26:06 +0000

    SEEM to have background processes able to run distinctly from normal processes. TODO: make sure BG processes are actually running as expected, and get a linked list going to process information.

--- inf.c
+++ inf.c
@@ -4,13 +4,15 @@
 
 int main(int argc, char* argv[])
 {
+	int i = 0;
 	if (argc != 3) {
 		fprintf(stderr, "Usage: inf tag interval\n");
 	} else {
 		const char* tag = argv[1];
 		int interval = atoi(argv[2]);
-		while(1) {
-			printf("%s\n", tag);
+		while(i<=10) {
+			i++;
+			printf("tag: %s, tick:%d\n", tag,i);
 			sleep(interval);
 		}
 	}
--- sample.c
+++ sample.c
@@ -9,62 +9,91 @@
 
 /*Prototypes*/
 char* getWorkingDirectory(int);
-void parseInput(char*,char***,size_t*);
-void executeCommand(char**);
+void parseInput(char*,char***,size_t*,int*);
+int executeCommand(char**,int*);
+//int executeBackgroundCommand(char**);
 
 int main()
 {
 	char* prompt;
 	char** args;
 	size_t argc;
-
-	int bailout = 0;
-	while (!bailout) {
+	int bg_flag = 0;
+	int running;
+	do {
 		prompt = getWorkingDirectory(0);
 		char* input = readline(prompt);
-		/* Note that readline strips away the final \n */
+		bg_flag = 0;
+		if( strcmp(input,"") ) {
+			parseInput(input,&args,&argc,&bg_flag);
+			running = executeCommand(args,&bg_flag);
+			//else { running = executeBackgroundCommand(args); }
 
-		if (!strcmp(input, "logout")) {
-			bailout = 1;
-		}
-		else {
-			parseInput(input,&args,&argc);
-			executeCommand( args );	
-			/*REFACTOR THIS LATER but useful for the moment*/
-			/*Free the args (might do in fn later)*/
 			int i;
 			for( i = 0; i < argc; i++) {
 				free( args[i] );
 			}
-			free( args );
+			free(args);
+			free(input);
+			free(prompt);
 		}
-		
-		free(input);
-		free(prompt);	
-	}
+	} while( running );
+
 	printf("Session closed\n");
 }
 
-void executeCommand( char** args ) {
+// int executeBackgroundCommand( char** args ) {
+// 	printf("Executing background command...\n");
+// 	/*Want to run chdir() since cd is not in bin*/
+// 	if( !strcmp( args[0], "cd" ) ) { //default cd with no args is go home
+// 		char* cwd = getWorkingDirectory(1);
+// 		if( NULL == args[1] || !strcmp(args[1], "~") ) {
+// 			printf("You ran CD in the background...this isn't running in the background - FIX!\n");
+// 			chdir( getenv("HOME") );
+// 		}
+// 		else {
+// 			printf("You ran CD in the background...this isn't running in the background - FIX!\n");
+// 			chdir(args[1]);
+// 		}
+// 		free(cwd);
+// 	}
 
+// 	else {
+// 		//RUN IN BACKGROUND! User entered an & symbol
+// 		//So now we use waitpid( -1, &status, WNOHANG )
+// 		pid_t child_pid;
+// 		pid_t w;
+// 		int status;//of child process
+// 		child_pid = fork();
+
+// 		if ( child_pid == 0 ) { //Child executes this
+// 			execvp( args[0], args );
+// 			/*PROGRAM IS OVERWRITTEN AFTER THIS POINT*/
+// 			/*execvp failed if the following message is printed*/
+// 			printf("BG: Shouldn't have gotten here: Unknown Command (args** invalid).\n");
+// 			exit(EXIT_SUCCESS);
+// 		}
+// 		else if( child_pid == -1 ) { 
+// 			printf("failed to fork\n"); 
+// 			exit(EXIT_FAILURE);
+// 		}
+// 		else { //Parent executes this
+// 			do {
+// 				w = waitpid(child_pid, &status, WNOHANG);
+// 			} while( (!WIFEXITED(status)) && !WIFSIGNALED(status));
+// 		}
+// 	}
+// 	return 1;
+// }
+
+int executeCommand( char** args, int* bg_flag ) {
+	printf("Executing normal command.\n");
 	/*Want to run chdir() since cd is not in bin*/
 	if( !strcmp( args[0], "cd" ) ) { //default cd with no args is go home
 		char* cwd = getWorkingDirectory(1);
 		if( NULL == args[1] || !strcmp(args[1], "~") ) {
 			chdir( getenv("HOME") );
 		}
-		// else {//if( !strcmp( args[1], "..") ) {
-		// 	char* relativePath;
-		// 	relativePath = (char*)malloc( strlen(cwd)+strlen(args[1])+2 );
-		// 	relativePath[0] = '\0';
-		// 	strncat( relativePath, cwd, strlen(cwd)+1 );
-		// 	strncat( relativePath, "/", 2 );
-		// 	strncat( relativePath, args[1], strlen(args[1])+1 );
-		// 	printf("!!relativePath val: %s\n", relativePath );
-		// 	chdir(relativePath);
-		// 	//free(cwd);
-		// 	free(relativePath);
-		// }
 		else {
 			//do something
 			//printf("recognized command chdir(). CWD is: %s\n", cwd);
@@ -73,39 +102,40 @@ void executeCommand( char** args ) {
 		}
 		free(cwd);
 	}
-	/*Use a program in binaries*/
+
+	/*Use a NON-BACKGROUND program in binaries*/
 	else {
 		pid_t child_pid;
+		pid_t w;
 		int status;//of child process
-			child_pid = fork();
-		if( child_pid == -1 ) { 
+		child_pid = fork();
+
+		if ( child_pid == 0 ) { //Child executes this
+			execvp( args[0], args );
+			/*PROGRAM IS OVERWRITTEN AFTER THIS POINT*/
+			/*execvp failed if the following message is printed*/
+			printf("NON-BG: Shouldn't have gotten here: Unknown Command (args** invalid).\n");
+			exit(EXIT_SUCCESS);
+		}
+		else if( child_pid == -1 ) { 
 			printf("failed to fork\n"); 
 			exit(EXIT_FAILURE);
 		}
-		if ( child_pid == 0 ) { //Child executes this
-			printf("Forked, attempting to call execvp()...\n");
-			execvp( args[0], args );
-			/*execvp failed if this point is reached*/
-				printf("Unknown command or args** is an invalid format...\n");
-				exit(EXIT_SUCCESS);
-		}
 		else { //Parent executes this
-			printf("PARENT process is running...\n");
-
-			/*Example from man pages for "wait"(if this works)*/
 			do {
-				pid_t w = wait(&status);
-				if (w == -1) {
-	               perror("waitpid");
-	               exit(EXIT_FAILURE);
-	           }
-	           if (WIFEXITED(status)) {
-	               printf("exited, status=%d\n", WEXITSTATUS(status));
-	               //exit(EXIT_SUCCESS);
-	           } 
-			} while ( !WIFEXITED(status) );//&& !WIFSIGNALED(status) );
-		} 	
+				//If we're running a background process...
+				if( (*bg_flag) ) {
+					w = waitpid(child_pid, &status, WNOHANG);
+				}
+				//else we're running a normal process where we just wait
+				else {
+					w = waitpid(child_pid, &status, WUNTRACED);
+				}
+				
+			} while( (!WIFEXITED(status)) && !WIFSIGNALED(status));
+		}
 	}
+	return 1;
 }
 
 /* parseIput( char* )
@@ -115,7 +145,7 @@ void executeCommand( char** args ) {
 *  behavior. There was likely better way to do this and it might be
 *  improved in future if there's time.
 */
-void parseInput( char* input, char*** args, size_t* argc ) {
+void parseInput( char* input, char*** args, size_t* argc, int* bg_flag ) {
 
 	/*init args to hold one string*/
 	(*args) = (char**)malloc( sizeof(char*) );
@@ -125,7 +155,6 @@ void parseInput( char* input, char*** args, size_t* argc ) {
 
 	/*Read the string and tokenize it into individual words delimited by spaces*/
 	char* token;
-	//char** args = (char**)malloc( sizeof(char*)*(*argc) );
 
 	const char* delimeter = " ";
 
@@ -133,20 +162,27 @@ void parseInput( char* input, char*** args, size_t* argc ) {
 	token = strtok( input, delimeter );
 	/*Remaining tokens*/
 	while( token != NULL ) {
-		//printf( "TOKEN IS: %s\n", token );
+		printf( "TOKEN IS: %s\n", token );
 
-		//Put each token into the args array
-		//Grow args if tokens will exceed the length of array
-		if( (num_tokens+1) > (*argc) ) {
-			//printf("num_tokens exceeded the size of the argument array\n");
-			(*argc)++;
-			//printf("size args: %zu\n", (*argc));
-			(*args) = (char**)realloc( (*args), sizeof(char*)*(*argc) );
+		//Test if very first token is bg so we can ignore it and set the bg_flag
+		//bg_flag will cause executeCommand() to run a child process in the background
+		if( !strcmp(token,"bg") && num_tokens == 0 ) { printf("Setting bg_flag to TRUE...\n");(*bg_flag)=1; }
+		
+		//Otherwise parse out the arguments list as usual
+		else {
+			//Put each token into the args array
+			//Grow args if tokens will exceed the length of array
+			if( (num_tokens+1) > (*argc) ) {
+				//printf("num_tokens exceeded the size of the argument array\n");
+				(*argc)++;
+				//printf("size args: %zu\n", (*argc));
+				(*args) = (char**)realloc( (*args), sizeof(char*)*(*argc) );
+			}
+			//printf("attempting to malloc\n");
+			(*args)[num_tokens] = (char*)malloc( sizeof(char)*(strlen(token)+1) );
+			strncpy( (*args)[num_tokens], token, strlen(token)+1 );
+			num_tokens++;
 		}
-		//printf("attempting to malloc\n");
-		(*args)[num_tokens] = (char*)malloc( sizeof(char)*(strlen(token)+1) );
-		strncpy( (*args)[num_tokens], token, strlen(token)+1 );
-		num_tokens++;
 		token = strtok( NULL, delimeter );
 	}
 	(*argc)++;
@@ -204,32 +240,3 @@ char* getWorkingDirectory( int returnDirectoryOnly ) {
 	/*Can return the prompt string with cwd or just the cwd ( useful for chdir() )*/
 	return finalPrompt;	
 }
-
-// char* getHomeDirectory( ) {
-// 	char* buf = NULL;
-// 	/*Note that ptr_buf points to the actual string*/
-// 	char* ptr_buf = NULL;
-// 	int allocated = 0;
-// 	size_t buffer_multiplier = 8;
-// 	size_t fullsize = 0;
-// 	char* finalPrompt = NULL;
-
-// 	We want the prompt to display a current working directory of any size
-// 	So the loops continues to double the buffer_multipler by 2 on each iteration
-// 	until system command getcwd() is successful. We will know if getcwd fails because
-// 	the string pointer which contains the cwd will remain null if unsucessful
-// 	while( ptr_buf == NULL ) {
-// 		buffer_multiplier *= 2;
-// 		ptr_buf = getenv( buf, buffer_multiplier );
-// 	}
-// 	/**/
-
-// 	fullsize = strlen(ptr_buf);
-// 	finalPrompt = (char*)malloc( fullsize+1 );
-// 	finalPrompt[0] = '\0';
-// 	strncat( finalPrompt, ptr_buf, fullsize );
-// 	free(ptr_buf);
-	
-// 	/*Can return the prompt string with cwd or just the cwd ( useful for chdir() )*/
-// 	return finalPrompt;	
-// }
